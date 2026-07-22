f8fbd1ae0fc383d2e7e1271e1a9c9525fdc6c8c5
Katsudon27 <katsudon1127@gmail.com>
2026-07-22 18:00:15 +1000

    feat(square): add #add_piece
    Contains initialisation of relevant variables for the object

---
+++ lib/square.rb
@@ -0,0 +1,17 @@
+require_relative "coordinates"
+
+# A class that is used to represent a square of the chess board
+class Square
+  attr_reader :symbol, :piece
+
+  def initialize(symbol, row, col)
+    @symbol = symbol
+    @coordinates = Coordinates.new(row, col)
+    @piece = nil
+    @possible_moves = []
+  end
+
+  def add_piece(piece)
+    @piece = piece
+  end
+end
