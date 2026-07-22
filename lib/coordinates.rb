819ca2a08250945a7a87907eccbe47f4f381d551
Katsudon27 <katsudon1127@gmail.com>
2026-07-22 16:43:19 +1000

    feat(coordinates): add implementation for #to_s
    Algebraic notation is used to represent the coordinates of each square

---
+++ lib/coordinates.rb
@@ -0,0 +1,22 @@
+# A class used to represent the coordinates of a square on the chess board
+class Coordinates
+  attr_reader :row, :column
+
+  def initialize(row, column)
+    @row = row
+    @column = column
+  end
+
+  def to_s
+    column_mapping[@column] + @row.to_s
+  end
+
+  private
+
+  def column_mapping
+    columns = ("a".."h").to_a
+    hash = {}
+    (0..7).each { |num| hash[num] = columns[num] }
+    hash
+  end
+end
