7b0c20bd9fd586ab4de8a569dae727041cd7de5a
Katsudon27 <katsudon1127@gmail.com>
2026-07-22 17:30:03 +1000

    test(coordinates): add unit test for #+(other)

--- spec/coordinates_spec.rb
+++ spec/coordinates_spec.rb
@@ -10,4 +10,14 @@ describe Coordinates do
       end
     end
   end
+
+  describe "+(other)" do
+    context "when added with an offset of [1,1]" do
+      it "moves to b1" do
+        offset = described_class.new(1, 1)
+        result = coordinates + offset
+        expect(result.to_s).to eq("b1")
+      end
+    end
+  end
 end
