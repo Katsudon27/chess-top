1f4566e05cc43bf1970a46d269d6e9f3e5e407a5
Katsudon27 <katsudon1127@gmail.com>
2026-07-23 15:46:12 +1000

    feat: add queen class

---
+++ lib/pieces/queen.rb
@@ -0,0 +1,9 @@
+require_relative "piece"
+
+# A class to represent the Bishop piece on a chess board
+class Queen < Piece
+  def initialize(symbol)
+    super(symbol)
+    @movement_offsets = [[1, 1], [-1, 1], [1, -1], [-1, -1], [1, 0], [-1, 0], [0, 1], [0, -1]]
+  end
+end
