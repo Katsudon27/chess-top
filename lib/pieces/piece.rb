20b3ff07cf965b578c74a89a951a7bc95a62acb0
Katsudon27 <katsudon1127@gmail.com>
2026-07-23 15:32:38 +1000

    refactor: move #possible_moves to piece class
    This is because the rook, bishop and queen all can move without a limit
    on the distance as long as they stay within the boundaries of the chess
    board thus it makes more sense to use it as a default implementation of
    the method in the parent class.

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -5,4 +5,21 @@ class Piece
   def initialize(symbol)
     @symbol = symbol
   end
+
+  def possible_moves(current_position)
+    results = []
+    current_coordinates = convert_notation(current_position)
+    @movement_offsets.each do |movement_offset|
+      movement = Coordinates.new(movement_offset[0], movement_offset[1])
+      temp = current_coordinates
+      loop do
+        move = temp + movement
+        break unless move.valid?
+
+        results << move.to_s
+        temp = move
+      end
+    end
+    results
+  end
 end
--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -9,21 +9,4 @@ class Rook < Piece
     super(symbol)
     @movement_offsets = [[1, 0], [-1, 0], [0, 1], [0, -1]]
   end
-
-  def possible_moves(current_position)
-    results = []
-    current_coordinates = convert_notation(current_position)
-    @movement_offsets.each do |movement_offset|
-      movement = Coordinates.new(movement_offset[0], movement_offset[1])
-      temp = current_coordinates
-      loop do
-        move = temp + movement
-        break unless move.valid?
-
-        results << move.to_s
-        temp = move
-      end
-    end
-    results
-  end
 end
