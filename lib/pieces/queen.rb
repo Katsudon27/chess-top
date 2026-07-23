2e5d607e8a1b58842491157401d56ff6f9c883a0
Katsudon27 <katsudon1127@gmail.com>
2026-07-23 15:57:11 +1000

    doc: fix comment typo

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,6 +1,6 @@
 require_relative "piece"
 
-# A class to represent the Bishop piece on a chess board
+# A class to represent the Queen piece on a chess board
 class Queen < Piece
   def initialize(symbol)
     super(symbol)
