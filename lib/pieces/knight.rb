50c6196afa042baad5468b0ac69b20ccda2fe387
Katsudon27 <katsudon1127@gmail.com>
2026-07-23 12:28:09 +1000

    feat(knight): add #possible_moves
    Calculate and store the possible moves that can be made from a position
    as algebraic notations

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,9 +1,24 @@
 require_relative "piece"
+require_relative "../coordinates"
+require_relative "../conversion"
 
 # A class to represent the Knight piece on a chess board
 class Knight < Piece
+  include Conversion
   def initialize(symbol)
     super(symbol)
-    @move_constants = [[1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [-2, 1], [2, -1], [2, -2]]
+    @movement_offsets = [[2, 1], [2, -1], [1, 2], [1, -2], [-2, 1], [-2, -1], [-1, 2], [-1, -2]]
+  end
+
+  def possible_moves(current_position)
+    results = []
+    current_coordinates = convert_notation(current_position)
+    @movement_offsets.each do |movement_offset|
+      movement = Coordinates.new(movement_offset[0], movement_offset[1])
+      move = current_coordinates + movement
+
+      results << move.to_s if move.valid?
+    end
+    results
   end
 end
