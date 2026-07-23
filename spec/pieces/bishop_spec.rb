575e4a8a4da97443cc1110705576fb954b080699
Katsudon27 <katsudon1127@gmail.com>
2026-07-23 15:32:11 +1000

    fix: fix typo for passed argument

--- spec/pieces/bishop_spec.rb
+++ spec/pieces/bishop_spec.rb
@@ -6,7 +6,7 @@ describe Bishop do
   describe "#possible_moves" do
     context "when the piece is at d4" do
       it "returns an array of coordinates of possible moves that can be made" do
-        expect(rook.possible_moves("e4").sort).to eq(%w[c5 b6 a7 a1 b2 c3 e3 f2 g1 e5 f6 g7 h8].sort)
+        expect(bishop.possible_moves("d4").sort).to eq(%w[c5 b6 a7 a1 b2 c3 e3 f2 g1 e5 f6 g7 h8].sort)
       end
     end
   end
