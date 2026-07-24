65beeea4950e6639bc449f770264b87f43b29f29
Katsudon27 <katsudon1127@gmail.com>
2026-07-24 10:03:52 +1000

    feat: add pawn class

---
+++ lib/pieces/pawn.rb
@@ -0,0 +1,11 @@
+require_relative "piece"
+
+# A class to represent the Pawn piece on a chess board
+class Pawn < Piece
+  def initialize(symbol)
+    super(symbol)
+    @movement_offsets = [[0, 1]]
+    @max_distance = 2
+    @moved = false
+  end
+end
