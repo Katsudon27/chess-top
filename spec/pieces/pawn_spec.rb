fe13ebf7b6755eaaeac478959ba7f966252bc543
Katsudon27 <katsudon1127@gmail.com>
2026-07-24 10:21:57 +1000

    fix: update pawn state for second unit test
    Also fixed typo for variable name

--- spec/pieces/pawn_spec.rb
+++ spec/pieces/pawn_spec.rb
@@ -6,13 +6,17 @@ describe Pawn do
   describe "#possible_moves" do
     context "when the piece is at original position of d2" do
       it "can take one or two step forward" do
-        expect(king.possible_moves("d2")).to eq(%w[d3 d4])
+        expect(pawn.possible_moves("d2")).to eq(%w[d3 d4])
       end
     end
 
     context "when the piece is at d3" do
+      before do
+        pawn.already_moved
+      end
+
       it "can only take one step forward" do
-        expect(king.possible_moves("d2")).to eq(%w[d3 d4])
+        expect(pawn.possible_moves("d3")).to eq(%w[d4])
       end
     end
   end
