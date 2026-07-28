0d0b1db5052f61dfb16c9f0287e46f141804f5e5
Katsudon27 <katsudon1127@gmail.com>
2026-07-28 11:27:45 +1000

    fix(board): fix pawn symbols and pieces' location
    - Updated the pawn symbols for black and white
    - Swapped the rows for black and white pieces

--- lib/board.rb
+++ lib/board.rb
@@ -12,14 +12,23 @@ class Board
   include Conversion
   attr_accessor :board
 
-  WHITE_PIECES = { king: "\u2654", queen: "\u2655", rook: "\u2656", bishop: "\u2657", knight: "\u2658" }.freeze
-  BLACK_PIECES = { king: "\u265A", queen: "\u265B", rook: "\u265C", bishop: "\u265D", knight: "\u265E" }.freeze
+  WHITE_PIECES = { king: "\u2654", queen: "\u2655", rook: "\u2656", bishop: "\u2657", knight: "\u2658",
+                   pawn: "\u2659" }.freeze
+  BLACK_PIECES = { king: "\u265A", queen: "\u265B", rook: "\u265C", bishop: "\u265D", knight: "\u265E",
+                   pawn: "\u265F" }.freeze
 
   def initialize
     @board = []
     setup_board
-    setup_pieces(7, 6, BLACK_PIECES)
-    setup_pieces(0, 1, WHITE_PIECES)
+    setup_pieces(0, 1, BLACK_PIECES)
+    setup_pieces(7, 6, WHITE_PIECES)
+  end
+
+  def display
+    @board.each_with_index do |row, row_idx|
+      puts "#{column_mapping[7 - row_idx]} #{row.map(&:symbol).join('')}"
+    end
+    puts "   1  2  3  4  5  6  7  8"
   end
 
   private
@@ -29,7 +38,7 @@ class Board
       row = []
       8.times do |col_idx|
         row << if (row_idx + col_idx).even?
-                 Square.new("black", col_idx, row_idx)
+                 Square.new("green", col_idx, row_idx)
                else
                  Square.new("white", col_idx, row_idx)
                end
@@ -39,7 +48,7 @@ class Board
   end
 
   def setup_pieces(start_row, pawn_row, pieces)
-    @board[pawn_row].each { |square| square.add_piece(Pawn.new("\u265F")) }
+    @board[pawn_row].each { |square| square.add_piece(Pawn.new(pieces[:pawn])) }
     @board[start_row][0].add_piece(Rook.new(pieces[:rook]))
     @board[start_row][1].add_piece(Knight.new(pieces[:knight]))
     @board[start_row][2].add_piece(Bishop.new(pieces[:bishop]))
