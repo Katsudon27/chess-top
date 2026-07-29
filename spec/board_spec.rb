5bd1b5aa441c9c035831b8592438de7ba04b98df
Katsudon27 <katsudon1127@gmail.com>
2026-07-29 12:00:18 +1000

    test(board): add unit test for #move_piece

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,28 @@
 require_relative "../lib/board"
+require_relative "../lib/pieces/knight"
 
 describe Board do
   subject(:chess_board) { described_class.new }
+
+  let(:board) { chess_board.instance_variable_get(:@board) }
+
+  describe "#move_piece" do
+    context "when pawn at e2 is moved to e4" do
+      let(:start) { board[6][4] }
+      let(:target) { board[4][4] }
+      let(:knight) { instance_double(Knight) }
+
+      before do
+        allow(start).to receive(:piece).and_return(knight)
+        allow(target).to receive(:add_piece)
+      end
+
+      it "updates the board" do
+        chess_board.move_piece("e2", "e4")
+        expect(target).to have_received(:add_piece).once
+      end
+    end
+  end
 
   describe "#export" do
     context "when at starting position" do
