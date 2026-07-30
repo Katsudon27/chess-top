639cf7feda4efa24fca69695087649ac103ff103
Katsudon27 <katsudon1127@gmail.com>
2026-07-30 12:01:29 +1000

    test(gc): add unit test for #switch_turn

---
+++ spec/game_controller_spec.rb
@@ -0,0 +1,21 @@
+require_relative "../lib/game_controller"
+require_relative "../lib/board"
+require_relative "../lib/player"
+require_relative "../lib/square"
+
+describe GameController do
+  subject(:game_controller) { described_class.new(first_player, second_player, game_board) }
+
+  let(:first_player) { instance_double(Player) }
+  let(:second_player) { instance_double(Player) }
+  let(:game_board) { instance_double(Board) }
+
+  describe "#switch_turn" do
+    context "when the current player is player 1" do
+      it "switches to the other player as the current player" do
+        game_controller.switch_turn
+        expect(game_controller.instance_variable_get(:@current_player)).to eq(second_player)
+      end
+    end
+  end
+end
