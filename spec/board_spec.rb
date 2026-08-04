a30a5fc52c2a3f8659dd2f14c41b6805d0e6c839
Katsudon27 <katsudon1127@gmail.com>
2026-08-04 16:53:06 +1000

    test(board): add test for #populate_possible_moves

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,21 +1,20 @@
 require_relative "../lib/board"
-require_relative "../lib/pieces/knight"
+require_relative "../lib/pieces/pawn"
 
 describe Board do
   subject(:chess_board) { described_class.new(empty_board) }
 
   let(:empty_board) { described_class.setup_board }
-
   let(:board) { chess_board.instance_variable_get(:@board) }
 
   describe "#move_piece" do
     context "when pawn at e2 is moved to e4" do
       let(:start) { board[6][4] }
       let(:target) { board[4][4] }
-      let(:knight) { instance_double(Knight) }
+      let(:pawn) { instance_double(Pawn) }
 
       before do
-        allow(start).to receive(:piece).and_return(knight)
+        allow(start).to receive(:piece).and_return(pawn)
         allow(target).to receive(:add_piece)
       end
 
@@ -46,12 +45,36 @@ describe Board do
     end
   end
 
-  describe "#import" do
+  describe "self.#import" do
     context "when called with a notation of move 1.e4 from starting position" do
       it "changes the current board positions to match the provided notation" do
         answer = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR"
         test_board = described_class.import(answer)
         expect(test_board.export).to eq(answer)
+      end
+    end
+  end
+
+  describe "#populate_possible_moves" do
+    context "when called on pawn at e2" do
+      let(:start) { board[6][4] }
+      let(:square1) { board[5][4] }
+      let(:pawn) { instance_double(Pawn) }
+
+      before do
+        allow(start).to receive(:piece).and_return(pawn)
+        allow(pawn).to receive(:possible_moves).and_return(%w[e3 e4])
+        allow(square1).to receive(:add_highlight)
+      end
+
+      it "calls #possible_moves on the pawn of e2" do
+        chess_board.populate_possible_moves("e2")
+        expect(pawn).to have_received(:possible_moves).once
+      end
+
+      it "calls #add_highlight on e3" do
+        chess_board.populate_possible_moves("e2")
+        expect(square1).to have_received(:add_highlight).once
       end
     end
   end
