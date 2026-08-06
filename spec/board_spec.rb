4b6ace176d15ffa0168cb4db0f383f7f0cdeedc8
Katsudon27 <katsudon1127@gmail.com>
2026-08-06 15:42:32 +1000

    test: add tests for #move_piece
    These tests are mainly for testing the assignment and reset of en
    passant square and the effects when an en passant capture is used or
    not used.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,25 +2,54 @@ require_relative "../lib/board"
 require_relative "../lib/pieces/pawn"
 
 describe Board do
-  subject(:chess_board) { described_class.new(empty_board) }
+  subject(:chess_board) { described_class.import("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") }
 
-  let(:empty_board) { described_class.setup_board }
   let(:board) { chess_board.instance_variable_get(:@board) }
 
   describe "#move_piece" do
-    context "when pawn at e2 is moved to e4" do
-      let(:start) { board[6][4] }
-      let(:target) { board[4][4] }
-      let(:pawn) { instance_double(Pawn) }
+    let(:target) { board[4][4] }
 
+    context "when pawn at e2 is moved to e4" do
       before do
-        allow(start).to receive(:piece).and_return(pawn)
         allow(target).to receive(:add_piece)
       end
 
       it "updates the board" do
+        chess_board.populate_possible_moves("e2")
         chess_board.move_piece("e2", "e4")
         expect(target).to have_received(:add_piece).once
+      end
+
+      it "updates the en passant square" do
+        chess_board.populate_possible_moves("e2")
+        chess_board.move_piece("e2", "e4")
+        expect(chess_board.en_passant_square).to eq("e3")
+      end
+    end
+
+    context "when there is an en passant square but the opponent does not take the pawn" do
+      it "removes the en passant square" do
+        chess_board.populate_possible_moves("e2")
+        chess_board.move_piece("e2", "e4")
+        chess_board.move_piece("g7", "g6")
+        expect(chess_board.en_passant_square).to eq("-")
+      end
+    end
+
+    context "when the opponent does an en passant capture" do
+      subject(:chess_board) { described_class.import("rnbqkbnr/ppp1pppp/8/8/3pP3/8/PPPP1PPP/RNBQKBNR") }
+
+      let(:board) { chess_board.instance_variable_get(:@board) }
+      let(:target_square) { board[4][4] }
+
+      before do
+        allow(target_square).to receive(:clear)
+      end
+
+      it "removes the piece from the target square" do
+        chess_board.en_passant_square = "e3"
+        chess_board.move_piece("d4", "e3")
+        expect(target_square).to have_received(:clear).once
       end
     end
   end
