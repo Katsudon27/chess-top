c854f23e21e21f29c079117d15cfb71f49f28565
Katsudon27 <katsudon1127@gmail.com>
2026-08-06 10:59:40 +1000

    feat(pawn): add movement logic for pawns
    - Delete moves that would result in the pawn moving to non-empty squares
    in front of it.
    - Add moves for pawns to attack enemy pieces that are diagonally in
    front of it.

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -6,9 +6,26 @@ class Pawn < Piece
     super(color)
     @symbol = "\u265F"
     @movement_offsets = [[0, 1]]
+    @attacking_offsets = [[1, 1], [-1, 1]]
     @max_distance = 2
     @moved = false
     @letter = "p"
+  end
+
+  def possible_moves(current_position, board)
+    results = super(current_position, board)
+
+    return results if @movement_offsets.length > 3
+
+    current_coordinates = convert_notation(current_position)
+
+    temp_results = results
+    temp_results.each do |result|
+      target_square = board.find_square(result)
+      results.delete(result) unless target_square.empty?
+    end
+
+    attacking_movements(current_coordinates, results, board)
   end
 
   def already_moved
@@ -30,5 +47,23 @@ class Pawn < Piece
       @movement_offsets = [[1, 1], [-1, 1], [1, -1], [-1, -1]]
       @max_distance = 8
     end
+  end
+
+  private
+
+  def attacking_movements(current_coordinates, results, board)
+    @attacking_offsets.each do |attacking_offset|
+      movement = Coordinates.new(attacking_offset[0], attacking_offset[1])
+      temp = current_coordinates
+
+      move = temp + movement
+      next unless move.valid?
+
+      square = board.find_square(move.to_s)
+      next if square.empty?
+
+      results << move.to_s if square.piece.color != @color
+    end
+    results
   end
 end
